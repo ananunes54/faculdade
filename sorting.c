f79c99eef5a6d595a486d3ac09fad0cc4681d31a
Ana Nunes <anacpn2006@gmail.com>
2025-01-10 20:59:11 -0300

    quick sort

--- sorting.c
+++ sorting.c
@@ -1,59 +1,72 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 void countingSort(int * list, int len);
 void quickSort(int * list, int len);
 
-int main()
-{
-	int list[] = {9, 3, 1, 7, 7, 0, 2};
-
-	countingSort(list, sizeof(list)/sizeof(int));
-
-	for (int i = 0; i < sizeof(list)/sizeof(int); i++) printf("%d, ", list[i]);
-	printf("\n");
-
-	return 0;
-}
-
 void countingSort(int * list, int len)
 {
-	int i = 0;
-	int largest = 0;
-	int i_position = 0;
-
-	int * list_cpy = malloc(sizeof(int) * len);
 	
+	int * cpy_list = malloc(sizeof(int)*len);
+	int greatest = 0;
+
+	for (int i = 0; i < len; i++)
+	{
+		cpy_list[i] = list[i];
+		if (list[i] > greatest)
+			greatest = list[i];
+	}
+
+	int * index = malloc(sizeof(int) * (greatest+1));
+	memset(index, 0, (greatest+1) * sizeof(int));
+
+	for (int i = 0; i < len; i++)
+	{
+		index[list[i]] = index[list[i]] + 1;
+	}
+
+	for (int i = 1; i <= greatest; i++)
+	{
+		index[i] += index[i - 1];
+        }
+	
+	for (int i = len - 1; i >= 0; i--)
+	{
+		list[index[cpy_list[i]] - 1] = cpy_list[i];
+		index[cpy_list[i]]--;
+	}
+
+	free(index);
+	free(cpy_list);
+}
+
+void quickSort(int * list, int len)
+{
+	int pivot = list[len-1];
+	int i = 0;
+	int j = -1;
+	int temp = 0;
+	int * first_half = 0;
+	int * second_half = 0;
+
 	for (i = 0; i < len; i++)
 	{
-		if (list[i] > largest)
-			largest = list[i];
-		
-		list_cpy[i] = list[i];
+		if (list[i] <= pivot)
+		{
+			j++;
+			temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+		}
 	}
 
-	int * frequency = malloc(sizeof(int) * (largest + 1));
-	memset(frequency, 0, (largest + 1) * sizeof(int));
-
-	for (i = 0; i < len; i++)
+	first_half = list;
+	second_half = list + j + 1;
+	
+	if (len >= 2)
 	{
-		frequency[list[i]] = frequency[list[i]] + 1;
+		quickSort(first_half, j);
+		quickSort(second_half, i-j-1);
 	}
-
-
-	for (i = 1; i <= largest; i++)
-	{
-		frequency[i] += frequency[i-1];
-	}
-
-	for (i = len - 1; i >= 0; i--)
-	{
-		i_position = frequency[list_cpy[i]] - 1;
-		list[i_position] = list_cpy[i];
-		frequency[list_cpy[i]] = frequency[list_cpy[i]] - 1;
-	}
-
-	free(frequency);
-	free(list_cpy);
 }
